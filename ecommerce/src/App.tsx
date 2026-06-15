eeb1d47b1b100f8c574b7e938760e34a0965f8b7
ingoose <manuelmunozrecabal@gmail.com>
2026-06-14 22:50:16 -0400

    feat:crea componente categoria

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Navbar from './assets/Component/Navbar/Navbar'
 import Product from './assets/Component/Product/Product'
+import { listProductos } from "./mocks/products.js"
 
 
 function App() {
@@ -11,7 +12,18 @@ function App() {
       <div>        
         <Navbar />
         <main>
-            <h1>Bienvenido a nuestra tienda en línea</h1>            
+            <h1>Bienvenido a nuestra tienda en línea</h1>
+            <div className="product-list">              
+              {listProductos.map((producto) =>(
+                <Product
+                  titulo={producto.title}
+                  precio={producto.price}
+                  imagen={producto.image}
+                  id={producto.id}
+                  categoria={producto.category}
+                />
+              ))}              
+            </div>            
         </main>
         
       </div>
